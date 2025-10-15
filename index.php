5a785d1b57f62dda19cf5140d2520b1c41df0c92
rh64s <cchelovekov64@gmail.com>
2025-10-15 16:26:06 +0700

    task completed

--- index.php
+++ index.php
@@ -1,23 +1,50 @@
-<?php
-$login = !empty($_GET['login']) ? $_GET['login'] : 'логин не передан!';
-$password = !empty($_GET['password']) ? $_GET['password'] : 'пароль не передан!';
-?>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>3. Основы разработки</title>
+    <title>5. Взаимодействие</title>
 </head>
 <body>
-<form action="/login.php" method="GET">
-    <label>
-        Логин <input type="text" name="login">
-    </label>
-    <br>
-    <label>
-        Пароль <input type="password" name="password">
-    </label>
-    <br>
-    <input type="submit" value="Войти">
-</form>
+<p>На вход подается строка из чисел, разделенных пробелами.
+    Найдите все числа, встречающиеся 2 и более раз. Выведите их в любом порядке, разделяя пробелами.</p>
+<?php
+function find_copies($array) {
+    $new_array = [];
+    foreach ($array as $item) {
+        if(array_key_exists($item, $new_array)) {
+            $new_array[$item]++;
+        } else {
+            $new_array[$item] = 1;
+        }
+    }
+    $line = '';
+    foreach ($new_array as $item => $count) {
+        $line .= $count > 1 ? $item . ' ' : '';
+    }
+    return $line;
+}
+$string_nums = explode(" ", "3 2 5 2 1 3 1 1 3");
+echo find_copies($string_nums);
+?>
+<br>
+<p>На вход подается строка из чисел, разделенных пробелами.
+    Найдите максимальное произведение двух чисел из этой строки.</p>
+<?php
+function max_mult($array) {
+    $max = 0;
+    foreach ($array as $item1) {
+        foreach ($array as $item2) {
+            if ($item1 != $item2) {
+                if ($item2 * $item1 > $max) {
+                    $max = $item2 * $item1;
+                }
+            }
+        }
+    }
+    return $max;
+}
+
+$string_nums = explode(" ", "1 2 3 4");
+echo max_mult($string_nums);
+?>
 </body>
 </html>
